3a8490a10758791ee38b134e437810422ae8f1ce
Dustin Washington <dwash96@gmail.com>
2026-04-21 20:21:41 -0400

    Bump Version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # Build cross-platform wheels for py-cymbal by downloading pre-compiled binaries
 # and packaging them into appropriate platform-specific wheels.
 
-VERSION="0.1.19"
+VERSION="0.1.20"
 CYMBAL_VERSION="0.11.6"
 BASE_URL="https://github.com/1broseidon/cymbal/releases/download/v${CYMBAL_VERSION}"
 
@@ -34,6 +34,19 @@ from setuptools import setup
 from setuptools.dist import Distribution
 import os
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            super().finalize_options()
+            self.root_is_pure = False
+        def get_tag(self):
+            python, abi, plat = super().get_tag()
+            return 'py3', 'none', plat
+    cmdclass = {'bdist_wheel': bdist_wheel}
+except ImportError:
+    cmdclass = {}
+
 class BinaryDistribution(Distribution):
     def has_ext_modules(foo):
         return True
@@ -42,6 +55,7 @@ plat_name = os.environ.get('WHEEL_PLATFORM_NAME', 'any')
 
 setup(
     distclass=BinaryDistribution,
+    cmdclass=cmdclass,
     options={'bdist_wheel': {'plat_name': plat_name}}
 )
 SETUPEOF
